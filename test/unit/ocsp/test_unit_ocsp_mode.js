9aba6e475d7057b2f1b9effbd1f20a0076152df4
sfc-gh-ext-simba-lb <lbarber@simba.com>
2022-12-20 09:09:33 -0800

    add test and isInitialized() check

--- test/unit/ocsp/test_unit_ocsp_mode.js
+++ test/unit/ocsp/test_unit_ocsp_mode.js
@@ -3,8 +3,10 @@
  */
 
 const GlobalConfig = require('../../../lib/global_config');
+const OcspResponseCache = require('../../../lib/agent/ocsp_response_cache');
 
 const assert = require('assert');
+const async = require('async');
 
 describe('OCSP mode', function ()
 {
@@ -24,5 +26,26 @@ describe('OCSP mode', function ()
     assert.equal(GlobalConfig.getOcspMode(), GlobalConfig.ocspModes.FAIL_CLOSED);
     GlobalConfig.setOcspFailOpen(true);
     done();
+  });
+
+  it('OCSP no cache file', function (done)
+  {
+    var cache = new OcspResponseCache.OcspResponseCache();
+    var cert = "testCertificate";
+    var response = "testResponse";
+
+    async.series(
+      [
+        function (callback)
+        {
+          // attempt to set entry when cache has not been initialized
+          cache.set(cert, response);
+          callback();
+        }
+      ],
+      function ()
+      {
+        done();
+      });
   });
 });
